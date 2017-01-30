2f379f6469dec9652ae8d247b15c8a513e7080bf
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-01-30 16:03:35 +0000

    Formatting

--- test/Spec.hs
+++ test/Spec.hs
@@ -29,29 +29,52 @@ type family WordType (n :: Nat) :: * where
   WordType 32 = Word32
   WordType 64 = Word64
 
-sameConvAs :: (Integral n, Integral m, Integral i, Show i) => Proxy n -> Proxy m -> i -> Property
+sameConvAs
+  :: (Integral n, Integral m, Integral i, Show i)
+  => Proxy n -> Proxy m -> i -> Property
 sameConvAs (_ :: Proxy n) (_ :: Proxy m) (x :: i)
   = ((fromIntegral :: n -> i) . (fromIntegral :: i -> n)) x
   === ((fromIntegral :: m -> i) . (fromIntegral :: i -> m)) x
 
-sameConvI :: (KnownNat n, Integral (IntType n)) => Proxy n -> Integer -> Property
-sameConvI (_ :: Proxy n) = sameConvAs (Proxy :: Proxy (Int n)) (Proxy :: Proxy (IntType n))
+sameConvI
+  :: (KnownNat n, Integral (IntType n))
+  => Proxy n -> Integer -> Property
+sameConvI (_ :: Proxy n)
+  = sameConvAs (Proxy :: Proxy (Int n)) (Proxy :: Proxy (IntType n))
 
-sameConvW :: (KnownNat n, Integral (WordType n)) => Proxy n -> Natural -> Property
-sameConvW (_ :: Proxy n) = sameConvAs (Proxy :: Proxy (Word n)) (Proxy :: Proxy (WordType n))
+sameConvW
+  :: (KnownNat n, Integral (WordType n))
+  => Proxy n -> Natural -> Property
+sameConvW (_ :: Proxy n)
+  = sameConvAs (Proxy :: Proxy (Word n)) (Proxy :: Proxy (WordType n))
 
-sameFncAs :: (Integral n, Integral m, Integral i, Show i) => (forall a. Integral a => a -> a -> a) -> Proxy n -> Proxy m -> i -> i -> Property
+sameFncAs
+  :: (Integral n, Integral m, Integral i, Show i)
+  => (forall a. Integral a => a -> a -> a)
+  -> Proxy n -> Proxy m -> i -> i -> Property
 sameFncAs f (_ :: Proxy n) (_ :: Proxy m) (x :: i) (y :: i)
   = (fromIntegral :: n -> i) ((f `on` (fromIntegral :: i -> n)) x y)
   === (fromIntegral :: m -> i) ((f `on` (fromIntegral :: i -> m)) x y)
 
-sameFncI :: (KnownNat n, Integral (IntType n)) => (forall a. Integral a => a -> a -> a) -> Proxy n -> Integer -> Integer -> Property
-sameFncI f (_ :: Proxy n) = sameFncAs f (Proxy :: Proxy (Int n)) (Proxy :: Proxy (IntType n))
+sameFncI
+  :: (KnownNat n, Integral (IntType n))
+  => (forall a. Integral a => a -> a -> a)
+  -> Proxy n -> Integer -> Integer -> Property
+sameFncI f (_ :: Proxy n)
+  = sameFncAs f (Proxy :: Proxy (Int n)) (Proxy :: Proxy (IntType n))
 
-sameFncW :: (KnownNat n, Integral (WordType n)) => (forall a. Integral a => a -> a -> a) -> Proxy n -> Natural -> Natural -> Property
-sameFncW f (_ :: Proxy n) = sameFncAs f (Proxy :: Proxy (Word n)) (Proxy :: Proxy (WordType n))
+sameFncW
+  :: (KnownNat n, Integral (WordType n))
+  => (forall a. Integral a => a -> a -> a)
+  -> Proxy n -> Natural -> Natural -> Property
+sameFncW f (_ :: Proxy n)
+  = sameFncAs f (Proxy :: Proxy (Word n)) (Proxy :: Proxy (WordType n))
 
-testAll :: Testable a => (forall n. (KnownNat n, Integral (WordType n), Integral (IntType n)) => Proxy n -> a) -> IO ()
+testAll
+  :: Testable a
+  => (forall n. (KnownNat n, Integral (WordType n), Integral (IntType n))
+      => Proxy n -> a)
+  -> IO ()
 testAll prop = do
   quickCheck (prop (Proxy :: Proxy 8))
   quickCheck (prop (Proxy :: Proxy 16))
